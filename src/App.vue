3ad3b44c18d45820326235fad00847842e771441
fulliam <fordevjob@gmail.com>
2025-03-30 18:33:59 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <Square color="black" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Square from './components/Square.vue'
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  background-color: #f0f0f0;
+}
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div class="sqare" :style="{ backgroundColor: color }" />
+</template>
+
+<script setup lang="ts">
+defineProps<{ color: string }>()
+</script>
+
+<style scoped>
+.sqare {
+  width: 20rem;
+  height: 20rem;
+}
+</style>
